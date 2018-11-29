2baf320dc8b10195e510424059697aadd0685ea2
unknown <1275919414@qq.com>
2018-11-29 21:56:14 +0800

    a motify

--- src/views/layout/components/sidebar.vue
+++ src/views/layout/components/sidebar.vue
@@ -3,7 +3,10 @@
     <el-menu :router="true" :collapse="collapse">
       <template v-for="(items, index) in routes">
         <el-submenu v-if="!items.hidden && items.children" :index="items.name" :key="index">
-          <template slot="title"><i :class="items.meta.icon"></i>{{items.meta.title}}</template>
+          <template slot="title">
+            <i :class="items.meta.icon"></i>
+            <span>{{items.meta.title}}</span>
+          </template>
           <template v-for="child in items.children">
               <el-menu-item v-if="child.hidden" :index="child.path" :key="child.path">{{child.meta.title}}</el-menu-item>
               <el-submenu v-else-if="child.children && !child.hidden" :index="child.path" :key="child.path">
