752d67d1ae535ce9641a66014e970117e01f0d4f
unknown <1275919414@qq.com>
2019-08-18 19:03:23 +0800

    500 404 build error 08182019

---
+++ src/views/error/404.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="no-found">
+    <el-card>
+      <div class="error-desc">
+        <h1 class="title">404</h1>
+        <p class="text">404-对不起！您访问的页面不存在！</p>
+        <p class="text">
+          <span class="el-icon-loading"></span>Opps...page not found!
+        </p>
+        <p class="text">你可以点击以下继续...</p>
+        <div class="link">
+          <el-button @click="goBack">返回上一层</el-button>
+          <el-button @click="goHome">返回首页</el-button>
+        </div>
+      </div>
+      <img src="https://raw.githubusercontent.com/VisionView/YNNUZC/master/src/assets/img/404.png" alt="" class="error-img">
+    </el-card>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    goBack () {
+      this.$router.go(-1)
+    },
+    goHome () {
+      this.$router.push({path: '/'})
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.no-found
+  position relative
+  height 100%
+  margin auto
+  background #f9f9f9 !important
+  .el-card
+    position absolute
+    top 0
+    bottom 0
+    left 0
+    right 0
+    height 400px
+    width 800px
+    margin auto
+    padding 60px 50px
+    .error-desc
+      float left
+      width 360px
+      .title
+        text-shadow 20px 10px 10px #999
+        font-size 72px
+      .text
+        margin-top 22px
+        font-size 16px
+      .link
+        margin-top 40px
+    .error-img
+      float right
+      width 400px
+</style>
---
+++ src/views/error/500.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="no-found">
+    <el-card>
+      <div class="error-desc">
+        <h1 class="title">500</h1>
+        <p class="text">500-对不起！内部服务器错误!</p>
+        <p class="text"><span class="second">{{Second}}s</span>后将跳转至登录界面</p>
+        
+      </div>
+      <img src="https://raw.githubusercontent.com/VisionView/YNNUZC/master/src/assets/img/404.png" alt="" class="error-img">
+    </el-card>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      Second: 5
+    }
+  },
+  methods: {
+    clearLogin () {
+      let count = 5
+      setTimeout(() => {
+        // 清空登录信息
+        window.localStorage.removeItem('token')
+        // 跳转登录界面
+        this.$router.push({path: '/login'})
+      }, 5000);
+    },
+    showTime () {
+      // 设置显示的时间
+      let I = setInterval(() => {
+        if (this.Second !==0) {
+          this.Second -= 1
+        }
+      }, 1000);
+    }
+  },
+  mounted () {
+    this.clearLogin()
+    this.showTime()
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.no-found
+  position relative
+  height 100%
+  margin auto
+  background #f9f9f9 !important
+  .el-card
+    position absolute
+    top 0
+    bottom 0
+    left 0
+    right 0
+    height 300px
+    width 800px
+    margin auto
+    padding 60px 50px
+    .error-desc
+      float left
+      width 360px
+      .title
+        text-shadow 20px 10px 10px #999
+        font-size 72px
+      .text
+        margin-top 22px
+        font-size 16px
+        .second
+          font-size 24px
+          letter-spacing 2px
+          color #f00
+      .link
+        margin-top 40px
+    .error-img
+      float right
+      width 400px
+</style>
